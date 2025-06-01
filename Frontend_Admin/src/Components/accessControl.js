d2448766fec3b3e0d15cd3be2a8e149b28c170c6
mukul-sharma-tech <muku0784@gmail.com>
2025-06-02 00:46:43 +0530

    Update accessControl.js

--- Frontend_Admin/src/Components/accessControl.js
+++ Frontend_Admin/src/Components/accessControl.js
@@ -1,1 +1,7 @@
-
+export const accessConfig = {
+    enquiries: { password: 'enquiry123' },
+    blogs: { password: 'blog123' },
+    destinations: { password: 'dest123' },
+    packages: { password: 'package123' },
+  };
+  
