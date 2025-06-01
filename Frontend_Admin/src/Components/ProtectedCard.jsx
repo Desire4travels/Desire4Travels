f25cd43aeffc85d23d4312fe91b2f1cee42864db
Desire4travels <desire4travels@gmail.com>
2025-06-02 00:37:26 +0530

    Update ProtectedCard.jsx

--- Frontend_Admin/src/Components/ProtectedCard.jsx
+++ Frontend_Admin/src/Components/ProtectedCard.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { accessConfig } from '../config/accessControl.js';
+import { accessConfig } from '../Config/accessControl.js';
 
 const AUTO_LOCK_TIME = 5 * 60 * 1000; // 5 minutes 
 
